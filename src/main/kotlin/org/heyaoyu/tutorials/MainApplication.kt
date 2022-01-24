a0f64ee07e1983fb9c3532b0a8ae543c395a4161
HeYaoyu <heyaoyu@kkworld.com>
2022-01-24 21:59:53 +0800

    fx

--- src/main/kotlin/org/heyaoyu/tutorials/MainApplication.kt
+++ src/main/kotlin/org/heyaoyu/tutorials/MainApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
+import java.text.SimpleDateFormat
+import java.util.*
 
 @SpringBootApplication(
     exclude = [
@@ -17,7 +19,9 @@ import org.springframework.boot.runApplication
 class DemoApplication
 
 fun main() {
+    val SDF = SimpleDateFormat("yyyy-MM-dd")
     println(DateUtil.dateStrToTs("2021-10-01"))
+    println(SDF.parse(SDF.format(Date(System.currentTimeMillis()))).time)
     println(DateUtil.tsToDateStr(1635782400000))
     runApplication<DemoApplication>()
 }
