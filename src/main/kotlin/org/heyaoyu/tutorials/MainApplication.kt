5d1605c9d9bd675eb4018c0db670a10ccb09f58b
HeYaoyu <heyaoyu@kkworld.com>
2022-02-10 14:24:33 +0800

    feign work...

--- src/main/kotlin/org/heyaoyu/tutorials/MainApplication.kt
+++ src/main/kotlin/org/heyaoyu/tutorials/MainApplication.kt
@@ -3,18 +3,23 @@ package org.heyaoyu.tutorials
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.hystrix.HystrixAutoConfiguration
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
 @SpringBootApplication(
     exclude = [
+        DataSourceAutoConfiguration::class,
+        DataSourceTransactionManagerAutoConfiguration::class,
         MongoAutoConfiguration::class,
         MongoDataAutoConfiguration::class,
-        DataSourceAutoConfiguration::class
+        HystrixAutoConfiguration::class,
     ]
 )
 // 发布或消费注册
